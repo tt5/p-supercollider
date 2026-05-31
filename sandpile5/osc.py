603528c030794b99e661ee1f1e53a09cc3941b8c
n <2585803+tt5@users.noreply.github.com>
2026-05-31 09:46:31 +0200

    rhythm

--- sandpile5/osc.py
+++ sandpile5/osc.py
@@ -5,6 +5,7 @@ import numpy as np
 from pythonosc import udp_client
 from pythonosc.dispatcher import Dispatcher
 from datetime import datetime
+import random
 
 dispatcher = Dispatcher()
 
@@ -84,20 +85,68 @@ def gen():
 getgen = gen()
 
 
-key = 48
-for i in range(240):
+keyTwo = 48
+for i in range(1000):
+    key = 38 - next(getgen_sturm_2)*2
+    #key = 40
+
+    #key = 48 - next(getgen_sturm_2)*6
     
     val2 = 4*next(getgen_sturm_3)
     val3 = 3*next(getgen)
-    key1 = 48 + next(getgen_sturm_eta) - next(getgen_sturm_2) + val2 + val3
-    key2 = 48 + next(getgen_sturm_eta) - next(getgen_sturm_2) + val2 + val3
-    key3 = 48 + next(getgen_sturm_eta) - next(getgen_sturm_2) + val2 + val3
-    key4 = 48 + next(getgen_sturm_eta) - next(getgen_sturm_2) + val2 + val3
-    osc_client.send_message("/key1", [key1,key1,key2,key2,key3,key3,key4,key4])
+
+    key1 = key + next(getgen_sturm_eta) - next(getgen_sturm_2) + val2 + val3
+    key2 = key + next(getgen_sturm_eta) - next(getgen_sturm_2) + val2 + val3
+    key3 = key + next(getgen_sturm_eta) - next(getgen_sturm_2) + val2 + val3
+    key4 = key + next(getgen_sturm_eta) - next(getgen_sturm_2) + val2 + val3
+    key5 = key + next(getgen_sturm_eta) - next(getgen_sturm_2) + val2 + val3
+    key6 = key + next(getgen_sturm_eta) - next(getgen_sturm_2) + val2 + val3
+    key7 = key + next(getgen_sturm_eta) - next(getgen_sturm_2) + val2 + val3
+    key8 = key + next(getgen_sturm_eta) - next(getgen_sturm_2) + val2 + val3
+
+    #rand = random.randint(0,1)
+    #rand = next(getgen_sturm_2)
+    rand = 1
+    #key1 = key1 * -(next(getgen_sturm_2)-1) * rand
+    #key2 = key2 * -(next(getgen_sturm_2)-1) * rand
+    #key3 = key3 * -(next(getgen_sturm_2)-1) * rand
+    #key4 = key4 * -(next(getgen_sturm_2)-1) * rand
+    #key5 = key5 * -(next(getgen_sturm_2)-1) * rand
+    #key6 = key6 * -(next(getgen_sturm_2)-1) * rand
+    #key7 = key7 * -(next(getgen_sturm_2)-1) * rand
+    #key8 = key8 * -(next(getgen_sturm_2)-1) * rand
+
+    key1 = key1 * -(next(getgen_sturm_2)-1) * rand
+    key2 = key2 * -(next(getgen_sturm_2)-1) * rand
+    key3 = key3 * -(next(getgen_sturm_2)-1) * rand
+    key4 = key4 * -(next(getgen_sturm_2)-1) * rand
+    key5 = key5 * -(next(getgen_sturm_2)-1) * rand
+    key6 = key6 * -(next(getgen_sturm_2)-1) * rand
+    key7 = key7 * -(next(getgen_sturm_2)-1) * rand
+    key8 = key8 * -(next(getgen_sturm_2)-1) * rand
+
+    test =  [34,34,34,34, 60,60,60,60]
+
+
+    rhy1=[]
+    #rhya = [2,0,2,0, 2,0,2,0]
+    rhy = [
+        [2,0,2,0,2,0,2,0],
+        [1,0,0,0,1,0,0,0],
+    ]
+    rhy1 = rhy[next(getgen_sturm_2)]
+
+    osc_client.send_message("/key1", [key1,key2,key3,key4,key5,key6,key7,key8])
+    osc_client.send_message("/key2", [key7+4,key8+4,key1+4,key2+4,key3+4,key4+4,key5+4,key6+4])
+    osc_client.send_message("/key3", [key5+7,key6+7,key7+7,key8+7,key1+7,key2+7,key3+7,key4+7])
+    #osc_client.send_message("/key1", test)
+    #osc_client.send_message("/key2", silence)
+    #osc_client.send_message("/key2", silence)
+    osc_client.send_message("/rhy1", rhy1)
     print("---")
     #print(next(getgen_sturm_eta))
     #print(next(getgen_sturm_2))
     #print(next(getgen_sturm_3))
     #print(next(getgen))
     print(key)
-    time.sleep(10)
+    time.sleep(105/120)
